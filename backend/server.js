a956cbdf463b5d697f329f4c34387b620533a97d
Sudhanshu Chauhan <sudhanshurajput29july-svg-sudhanshurajput29july@gmail.com>
2026-06-10 21:31:40 +0530

    i have changed .env and server.js  file in backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 // CORS config
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
